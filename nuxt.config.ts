0c46ef55827bf8d835d9e49ea5b4d2bf6d5e7944
yngrk <yngrk.kim@gmail.com>
2025-01-07 14:52:55 +0100

    fix error in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     // Options
@@ -9,5 +9,4 @@ export default defineNuxtConfig({
   css: [
     '@/assets/css/global.css'
   ],
-  devtools: { enabled: false }
 })
